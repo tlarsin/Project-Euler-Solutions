2dad98c4e19e6bddb12da45b5db9306baa7f2f7c
Tristan Larsin <tlarsin@gmail.com>
2017-07-23 16:41:42 -0500

    Solved Problem 2. Updated files and README.

---
+++ Problem2/main.py
@@ -0,0 +1,36 @@
+# Tristan Larsin
+# 7 - 23 - 17
+# Find the sum of the even valued Fibonacci numbers that don't exceed 4,000,000
+
+import sys
+import os
+
+def init():
+    fibList = []
+    count = 1
+    start = 1
+
+    fib1 = 0
+    fib2 = 0
+    sumVal = 0
+
+    upperBound = 4000000
+
+    while(sumVal < upperBound ):
+        i = len(fibList) - 1
+        val = 0
+
+        if(i + 1 < 1):
+            fibList.append(1)
+        if(i + 1 < 2):
+            fibList.append(2)
+        else:
+            val += fibList[i] + fibList[i - 1]
+            fibList.append(val)
+
+        if(fibList[i - 1] % 2 == 0):
+            sumVal += fibList[i - 1]
+
+    print('Sum of even values ' + str(sumVal))
+
+init()
--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Problems can be found at https://projecteuler.net/archives
 # Problems I have solved
 
 * Problem 1 - Multiples of 3 and 5
-
+* Problem 2 - Even Fibonacci numbers
 * Problem 4 - Largest Palindrome Product
 * Problem 5 - Smallest Multiple
 * Problem 6 - Sum Square Difference
